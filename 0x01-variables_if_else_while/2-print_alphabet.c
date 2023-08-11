252c6c00e45359823483d8b6be9dc2182dbba00b
stevewills <stephenonyekachikalu@gmail.com>
2023-08-11 13:47:41 -0700

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
 		putchar(letter);
 	}
+
 	putchar('\n');
 
 	return (0);
