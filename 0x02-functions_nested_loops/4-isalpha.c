64792b30583392ba3a60e494e7972fdabfc4a015
stevewills <stephenonyekachikalu@gmail.com>
2023-08-15 15:00:19 -0700

    Is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "math.h"
 /**
- * _isalpha - checks if character is an alphabet
+ * _isalpha - checks if character is alphabetic
  * @c: character to be checked
- * Return: 1 if character is a letter, lowercase or uppercase
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
