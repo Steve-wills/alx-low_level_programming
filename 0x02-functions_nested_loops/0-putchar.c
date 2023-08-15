4a38ae98a077b3b6282f87edceda36f6188aabae
stevewills <stephenonyekachikalu@gmail.com>
2023-08-15 10:09:12 -0700

    Output character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int _putchar(char p);
+
+int main() {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+int _putchar(char p) {
+	return write(1, &p, 1);
+}
