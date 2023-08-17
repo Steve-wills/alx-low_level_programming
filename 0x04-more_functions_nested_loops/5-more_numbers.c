e115723af162003effcdf49a4779b1fc22e9ca7c
stevewills <stephenonyekachikalu@gmail.com>
2023-08-17 06:42:03 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * more_numbers - prints numbers from 0 to 14 ten times
+ * followed by a new line
  */
 
 void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; ++x)
+	for (x = 0; x < 10; ++x)
 	{
 		for (y = 0; y <= 14; ++y)
 		{
